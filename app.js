c22e778e9ce083ab4886e28a647fac57f638f1ce
Vincent Handoyo <vincenthan28@gmail.com>
2024-07-31 16:53:31 +0800

    Updated app struct

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 require('dotenv').config(); 
 
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+
+//express app
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'views')));
+//connect to MongoDB
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI)
+.then((result) => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch(err => console.error(err));
 
+// Set up view engine and static files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
-
+  
 app.get('/news/:news_id', (req, res) => {
   const id = req.params.news_id;
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('news', { newsId: id });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
